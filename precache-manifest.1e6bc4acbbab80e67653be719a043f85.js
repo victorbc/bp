9e7359328a79d1fd3ecc35e4522ac788081fcb67
Victor Batista <vbatistacamilo@gmail.com>
2020-01-06 20:56:17 -0300

    Update page

---
+++ precache-manifest.1e6bc4acbbab80e67653be719a043f85.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b0ed3159568cf3582afebf581e291b",
+    "url": "/bp/index.html"
+  },
+  {
+    "revision": "bd14cfd852482e2dc182",
+    "url": "/bp/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "41ab0d8d5c1888360941",
+    "url": "/bp/static/css/main.bee09e6d.chunk.css"
+  },
+  {
+    "revision": "bd14cfd852482e2dc182",
+    "url": "/bp/static/js/2.80aa2f4b.chunk.js"
+  },
+  {
+    "revision": "41ab0d8d5c1888360941",
+    "url": "/bp/static/js/main.279db5fc.chunk.js"
+  },
+  {
+    "revision": "b0c93fbba9f4e5fbea6a",
+    "url": "/bp/static/js/runtime~main.d7da547d.js"
+  },
+  {
+    "revision": "a4976f47f11e617e5d0c919f95beaf9f",
+    "url": "/bp/static/media/box-01.a4976f47.jpg"
+  },
+  {
+    "revision": "99b6b43544bb99e037a781aa38e93bd7",
+    "url": "/bp/static/media/box-02.99b6b435.jpg"
+  },
+  {
+    "revision": "00c071744c1faa75ff537160a4cfc165",
+    "url": "/bp/static/media/box-03.00c07174.jpg"
+  },
+  {
+    "revision": "4d3bb9a095a1cb4c910710868a0b9398",
+    "url": "/bp/static/media/box-04.4d3bb9a0.jpg"
+  },
+  {
+    "revision": "44587affd0b5b1849076462784fe4a90",
+    "url": "/bp/static/media/box-05.44587aff.jpg"
+  },
+  {
+    "revision": "e4faff56e3099f991f9321e68daca047",
+    "url": "/bp/static/media/box-06.e4faff56.jpg"
+  },
+  {
+    "revision": "a1a26fe07c00e6403d400acc8b7b17d3",
+    "url": "/bp/static/media/logo.a1a26fe0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b541ec5d693f224af7f04c1aa4d9a14.js"
+  "/bp/precache-manifest.1e6bc4acbbab80e67653be719a043f85.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
